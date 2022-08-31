5a2087de68c8c6a603d7558b2dbdaadc7193871d
purushottam <psrawat23.pr@gmail.com>
2022-09-01 03:50:52 +0530

    created the django admin and model

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-#This projet contains django rest api api project
+# This projet contains django rest api api project
 
-#Vagrent is container based facility
+# Vagrent is container based facility
+
+Apiviews: describe logic to make api endpoint
+ http get post put patch delete  customize the function for all the menthod and give controle to write http method login
+-need full control over the login updating multiple datasource in one call
+
+- processing files and rendering a synchronous response
+- calling other api services
--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,58 @@
+from multiprocessing.sharedctypes import Value
+# from operator import mod
+# from pyexpat import model
+# from statistics import mode
+# from tkinter.tix import Tree
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 # Create your models here.
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles"""
+    def create_user(self,email,name,password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError('User must have an email address')
+        email = self.normalize_email(email)
+        user = self.model(email=email,name=name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self,email,name,password):
+        """new super user with given details"""
+        user=self.create_user(email,name,password)
+        user.is_superuser=True
+        user.is_staff=True
+        user.save(using=self._db)
+        return user
+        
+
+
+class UserProfile(AbstractBaseUser,PermissionsMixin):
+    """Database modell for user in the system """
+    email=models.EmailField(max_length=255,unique=True)
+    name=models.CharField(max_length=255)
+    is_active=models.BooleanField(default=True)
+    is_staff=models.BooleanField(default=False)
+    objects = UserProfileManager()
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=['name']
+
+    def get_full_name(self):
+        """Retrieving the full name"""
+        return self.name
+    
+    def get_short_name(self):
+        """retrieve short name of user"""
+        return self.name
+    
+    def __str__(self):
+        """return an object"""
+        return self.email
+
+
+
